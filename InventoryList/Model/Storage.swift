41c044b70a358221581fa14629693d8330211368
ilyabill <bilarusilya@gmail.com>
2022-01-04 14:42:22 +0300

    new model

--- InventoryList/Model/Storage.swift
+++ InventoryList/Model/Storage.swift
@@ -8,51 +8,58 @@
 import UIKit
 
 class Storage {
-
-    var items: [String: InventoryItem] = [
-        "macbook": InventoryItem(name: "macbook", quantity: 1, location: "home", image: UIImage(named: "macbook")!),
-        "arduino-nano" :InventoryItem(name: "arduino nano", quantity: 30, location: "work", image: UIImage(named: "arduino-nano")!),
-        "esp32": InventoryItem(name: "esp32", quantity: 50, location: "work", image: UIImage(named: "esp32")!),
-        "raspberry_pi_4": InventoryItem(name: "raspberry pi 4", quantity: 1, location: "Home", image: UIImage(named: "raspberry_pi_4")!),
-        "iphone_13": InventoryItem(name: "iphone 13", quantity: 1, location: "Home", image: UIImage(named: "iphone_13")!)
+    
+    var items: [InventoryItem] = [InventoryItem(name: "macbook", quantity: 1, location: "home", image: UIImage(named: "macbook")!),
+                                  InventoryItem(name: "arduino nano", quantity: 30, location: "work", image: UIImage(named: "arduino-nano")!),
+                                  InventoryItem(name: "esp32", quantity: 50, location: "work", image: UIImage(named: "esp32")!),
+                                  InventoryItem(name: "raspberry pi 4", quantity: 1, location: "Home", image: UIImage(named: "raspberry_pi_4")!),
+                                  InventoryItem(name: "iphone 13", quantity: 1, location: "Home", image: UIImage(named: "iphone_13")!)
     ]
     
     
+    // add new item
     func addNewItem(item: InventoryItem) throws {
-        if let _ = items[item.itemName] {
-            throw StorageError.storageHasThisItemName
+        if items.contains(where: { $0.itemName == item.itemName }) {
+            throw NSError(domain: "на складе уже есть предмет с таким именем", code: 2)
         } else {
-            items[item.itemName] = item
+            items.append(item)
         }
     }
     
+    //get item from name
+    func getItem(name: String) -> InventoryItem? {
+        if let index = items.firstIndex(where: { $0.itemName == name }) {
+            let item = items[index]
+            return item
+        }
+        return nil
+    }
     
+    
+    // get quantity by name
     func removeQuantity(itemName: String, quantity: Int) throws {
-        if let oldItem = items[itemName] {
-            oldItem.quantity = oldItem.quantity - quantity
-            if oldItem.quantity < 0 {
-                throw StorageError.itemsLessThan(deficiency: abs(oldItem.quantity))
+        if let itemIndex = items.firstIndex(where: { $0.itemName == itemName}) {
+            if items[itemIndex].quantity < quantity {
+                let missingPositions = abs(items[itemIndex].quantity - quantity)
+                throw NSError(domain: "нехватает \(missingPositions) шт", code: 1, userInfo: ["missingPositions": missingPositions])
             }
-            items.updateValue(oldItem, forKey: oldItem.itemName)
+            items[itemIndex].quantity = items[itemIndex].quantity - 1
         } else {
-            throw StorageError.notFindItem
+            throw NSError(domain: "нет предмета с таким именем на складе", code: 3)
         }
+        
     }
     
+    // add quattity by name
     func addQuantity(itemName: String, quantity: Int) throws {
-        if let oldItem = items[itemName] {
-            oldItem.quantity = oldItem.quantity + quantity
-            items.updateValue(oldItem, forKey: oldItem.itemName)
+        if let itemIndex = items.firstIndex(where: { $0.itemName == itemName}) {
+            items[itemIndex].quantity = items[itemIndex].quantity + 1
         } else {
-            throw StorageError.notFindItem
+            throw NSError(domain: "нет предмета с таким именем на складе", code: 3)
         }
+        
     }
     
 }
 
 
-enum StorageError: Error {
-    case itemsLessThan(deficiency: Int)
-    case notFindItem
-    case storageHasThisItemName
-}
--- InventoryList/ViewControllers/HomeViewController.swift
+++ InventoryList/ViewControllers/HomeViewController.swift
@@ -15,11 +15,41 @@ class HomeViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        do {
-            try storage.addNewItem(item: InventoryItem(name: "Звезда", quantity: 2, location: "home", image: UIImage(systemName: "star.fill")!))
-        } catch {
-            print("товар с таким именем существует, добавить не возможно")
+        
+        // получаем предмет по имени
+        if let item = storage.getItem(name: "macbook") {
+            print (item)
         }
+        
+        // пытаемся взять со склада 6 макбуков, хотя на складе только 1 шт, выведет ошибку, что не хватает 5 шт
+        do {
+            try storage.removeQuantity(itemName: "macbook", quantity: 6)
+        } catch {
+            let error = (error as NSError)
+            print(error.localizedDescription)
+            print(error.userInfo["missingPositions"] as! Int) // можно получить недостающее количество
+        }
+        
+        // пытаемся добавить новый предмет на склад в количестве 1000 штук
+        do {
+        try storage.addNewItem(item: InventoryItem(name: "star", quantity: 1000, location: "home", image: UIImage(systemName: "star.fill")!))
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        // получаем предмет по имени
+        if let item = storage.getItem(name: "star") {
+            print (item)
+        }
+        
+        //добавляем больше макбуков на склад
+        do {
+            try storage.addQuantity(itemName: "macbook", quantity: 10)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        
         
     }
     
